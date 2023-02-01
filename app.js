1479f7b3838a06a060542a3375f763496cec53e0
PatricioSant <pasantibanez.dwfsc5@bootcampudd.cl>
2023-02-01 17:15:29 -0300

    Java Script CRUD App

--- app.js
+++ app.js
@@ -14,8 +14,45 @@ const CrearItem = (plato) => {
   return item;
 };
 
-let mar = CrearItem("Ceviche");
-let fritos = CrearItem("Lomo a lo pobre");
-console.log(mar);
+const GuardarDB = () => {
+  localStorage.setItem("Platos", JSON.stringify(arrayPlatos));
+  PintarDB();
+};
 
-console.log(arrayPlatos);
+const PintarDB = () => {
+  listaPlatosUI.innerHTML = "";
+  arrayPlatos = JSON.parse(localStorage.getItem("Platos"));
+
+  if (arrayPlatos === null) {
+    arrayPlatos = [];
+  } else {
+    arrayPlatos.forEach((element) => {
+      listaPlatosUI.innerHTML += `
+    <div class="alert alert-success" role="alert">
+      <i class="material-symbols-outlined float-left mr-2"> restaurant </i>
+      ${element.plato} - ${element.estado}
+      <span class="float-right">
+        <i class="material-symbols-outlined"> done </i>
+        <i class="material-symbols-outlined"> delete </i>
+      </span>
+    </div>
+    `;
+    });
+  }
+};
+
+formularioUI.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let actividadUI = document.querySelector("#actividad").value;
+  console.log(actividadUI);
+  CrearItem(actividadUI);
+  GuardarDB();
+  formularioUI.reset();
+});
+
+document.addEventListener("DOMContentLoaded", PintarDB);
+
+listaPlatosUI.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(e);
+});
